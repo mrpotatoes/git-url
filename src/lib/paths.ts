8d8ee437e7e072e0c0174908cd5a4cab7ba7d4e4
ANDRIC LIBRESINN <Andric.Libresinn@libertymutual.com>
2021-05-10 16:01:08 -0400

    Initial conversion test

--- src/lib/paths.ts
+++ src/lib/paths.ts
@@ -1,20 +1,29 @@
 // TODO: Determine if git@github.com:rametta/pratica.git remote urls should work
 // TODO: Most of these functions rely on the function above. Let's try to fix that.
-import url from 'url'
+import { URL } from 'url'
+import { Either, left, right, tryCatch } from 'fp-ts/Either'
 
 // Get the provider host from the full git remote url.
 // TODO: This is temporary. Must return Left() or Right()
 export const providerHost = (gitUrl: string): string => {
   try {
-    return new url.URL(gitUrl).host
+    return new URL(gitUrl).host
   } catch (error) {
     return '---bad---'
   }
 }
 
+export const andric = (gitUrl: string): Either<string, string> => {
+  try {
+    return right(new URL(gitUrl).host)
+  } catch (error) {
+    return left('no')
+  }
+}
+
 // This will require some providerHost masaging.
 export const segments = (gitUrl: string): readonly string[] => (
-  new url.URL(gitUrl)
+  new URL(gitUrl)
     .pathname
     .replace('.git', '')
     .substring(1)
